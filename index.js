798c93e186fdc8fdb2aef15a71680b2500bd254f
Victor Lazaro <lazarofilm@gmail.com>
2021-01-03 17:11:51 -0500

    Baby steps...

---
+++ index.js
@@ -0,0 +1,8 @@
+const usEducationData =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json";
+const usCountyData =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json";
+
+console.log("Hello JS");
+
+d3.select("body").append("h2").text("Hello D3");
