dd19b10cfa4f1331017fdc3dc886983d120db88e
Victor Lazaro <lazarofilm@gmail.com>
2021-01-07 10:17:41 -0500

    commit

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const d3 = require("d3");
+const topojson = require("topojson-server");
+const topology = topojson.topology({ foo: geojson });
+
 const usEducationData =
   "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json";
 const usCountyData =
@@ -6,3 +10,13 @@ const usCountyData =
 console.log("Hello JS");
 
 d3.select("body").append("h2").text("Hello D3");
+
+// d3.geoAlbersUsa().scale(1300).translate([487.5, 305]);
+
+d3.append()
+  .enter()
+  .append("line")
+  .attr("x1", 100)
+  .attr("y1", 200)
+  .attr("x2", 100)
+  .attr("y2", 200);
