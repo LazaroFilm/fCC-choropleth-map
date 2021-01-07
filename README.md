e97c41c86b9f53aa828a4b60057541c9538b6884
Victor Lazaro <lazarofilm@gmail.com>
2021-01-07 17:39:01 -0500

    working on tooltip

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 - GitHub: https://github.com/LazaroFilm/fCC-choropleth-map
 - free Code Camp: https://www.freecodecamp.org/learn/data-visualization/data-visualization-projects/visualize-data-with-a-choropleth-map
+
+tutorial: https://youtu.be/ha1toFtBfF8
--- index.js
+++ index.js
@@ -1,22 +1,113 @@
-const d3 = require("d3");
-const topojson = require("topojson-server");
-const topology = topojson.topology({ foo: geojson });
-
-const usEducationData =
+const educationURL =
   "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json";
-const usCountyData =
+const countyURL =
   "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json";
 
-console.log("Hello JS");
+let countyData;
+let educationData;
 
-d3.select("body").append("h2").text("Hello D3");
+// d3.select("#title").append("h2").text("USA Education Data");
 
-// d3.geoAlbersUsa().scale(1300).translate([487.5, 305]);
+const canvas = d3.select("#canvas");
+const tooltip = d3.select("#tooltip");
+const legend = d3.select("#legend").attr("width", 130).attr("height", 80);
 
-d3.append()
-  .enter()
-  .append("line")
-  .attr("x1", 100)
-  .attr("y1", 200)
-  .attr("x2", 100)
-  .attr("y2", 200);
+// draws the map from the GeoJSON data
+let drawMap = () => {
+  canvas
+    .selectAll("paths")
+    .data(countyData)
+    .enter()
+    .append("path")
+    .attr("d", d3.geoPath())
+    .attr("class", "county")
+    .attr("fill", (countyDataItem) => {
+      let id = countyDataItem["id"];
+      let county = educationData.find((county) => {
+        return county["fips"] === id;
+      });
+      let percentage = county["bachelorsOrHigher"];
+      if (percentage <= 15) {
+        return "tomato";
+      } else if (percentage <= 30) {
+        return "orange";
+      } else if (percentage <= 45) {
+        return "lightgreen";
+      } else {
+        return "limegreen";
+      }
+    })
+    .attr("data-fips", (countyDataItem) => {
+      return countyDataItem["id"];
+    })
+    .attr("data-education", (countyDataItem) => {
+      let id = countyDataItem["id"];
+      let county = educationData.find((county) => {
+        return county["fips"] === id;
+      });
+      let percentage = county["bachelorsOrHigher"];
+      return percentage;
+    })
+    .on("mouseover", (countyDataItem) => {
+      tooltip.transition().style("visibility", "visible");
+      let id = countyDataItem["id"];
+      let county = educationData.find((county) => {
+        return county["fips"] === id;
+      });
+      console.log(county);
+      tooltip.text(
+        `${county["fips"]} - ${county["area_name"]}, ${county["state"]}: ${county["bachelorsOrHigher"]}%`
+      );
+    });
+};
+
+// takes the Data from the links and turns them into usable GeoJSON arrays
+// then calls for drawing the map form that data.A
+d3.json(countyURL).then((data, error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    countyData = topojson.feature(data, data.objects.counties).features;
+    // console.log("county:", countyData);
+    d3.json(educationURL).then((data, error) => {
+      if (error) {
+        console.log(error);
+      } else {
+        educationData = data;
+        // console.log("education:", educationData);
+        drawMap();
+      }
+    });
+  }
+});
+
+// // generating county fips
+// const countyFips = (countyDataItem) => {
+//   let id = countyDataItem["id"];
+//   let county = educationData.find((county) => {
+//     return county["fips"] === id;
+//   });
+//   console.log(county);
+//   return county;
+// };
+
+// making the legend
+const drawLegend = (position, color, range) => {
+  legend //
+    .append("rect")
+    .attr("x", 10)
+    .attr("y", position * 15)
+    .attr("width", 10)
+    .attr("height", 10)
+    .attr("stroke", "black")
+    .attr("fill", color);
+  legend //
+    .append("text")
+    .attr("x", 25)
+    .attr("y", position * 15 + 10)
+    .text(range);
+};
+drawLegend(1, "tomato", "less than 15");
+drawLegend(2, "orange", "15 to 30");
+drawLegend(3, "lightgreen", "20 to 45");
+drawLegend(4, "limegreen", "more than 45");
